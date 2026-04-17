8542fd4d73430550b870166842fe6880246e393e
Nathaniel Acquaah Arhin <nathanielarhin@gmail.com>
2026-04-17 01:55:34 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
+  .then(() => {
     console.log("MongoDB Connected");
 
     app.listen(PORT, () => {
-        console.log("Server running on port " + PORT);
+      console.log("Server running on port " + PORT);
     });
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.error("MongoDB Error:", err);
-});
+  });
